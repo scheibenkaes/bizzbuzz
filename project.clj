bc2903e693bfc4ca4d72750b7914335225533248
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-04-04 22:44:19 +0200

    Removed contrib

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject bizzbuzz "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+  :description "BizzBuzz"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
   :main bizzbuzz.core)

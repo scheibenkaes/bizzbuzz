26dc6876c8efb4ec290b2edcf911ef9393e0193f
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-04-04 18:41:14 +0200

    Working workflow

--- src/bizzbuzz/core.clj
+++ src/bizzbuzz/core.clj
@@ -1,1 +1,25 @@
-(ns bizzbuzz.core)
+(ns bizzbuzz.core
+  (use [clojure.string :only [join]])
+  (:gen-class))
+
+(defn bizz [n]
+  (if (zero? (mod n 3)) "Bizz" nil))
+
+(defn buzz [n]
+  (if (zero? (mod n 5)) "Buzz" nil))
+
+(defn bizzbuzz [n]
+  (let [bb (list (bizz n) (buzz n))
+        filtered (filter #(not (nil? %1)) bb)]
+    (if (empty? filtered) n (interleave "" filtered))))
+
+(defn bb-until [n]
+  (let [r (rest (range n))]
+    (map bizzbuzz r)))
+
+(defn bb [n]
+  (let [max-n (min n 1000)]
+    (bb-until max-n)))
+
+(defn -main [& args]
+  (println (bb 25)))
--- test/bizzbuzz/test/core.clj
+++ test/bizzbuzz/test/core.clj
@@ -2,5 +2,14 @@
   (:use [bizzbuzz.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest detects-bizz
+  (is (bizz 3)))
+
+(deftest bizz?-ignores-other-vals
+  (is (not (bizz 4))))
+
+(deftest buzz-with-multiples-of-5
+  (is (buzz 5)))
+
+(deftest buzz-with-no-multiple-of-5
+  (is (nil? (buzz 6))))
